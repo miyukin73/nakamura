eb6c45883c79b2458feb4fbe30c3d43e879c40ce
miyukin73 <nmiyuki73@keio.jp>
2020-07-09 21:22:50 +0900

    sudo_ap_on

--- setup/sudo_ap_off.sh
+++ setup/sudo_ap_off.sh
@@ -1,8 +1,19 @@
 #AP化OFF(２回目以降)
 #<<実行方法>>
 #bash sudo_ap_off.sh
-#network/interfacesファイルに以下の項目を追加
-sudo echo "source-directory /etc/network/interfaces.d\nauto lo\niface lo inet loopback\niface eth0 inet manual\nauto wlan0\nallow-hotplug wlan0\niface wlan0 inet static\naddress 192.168.7.1\nnetmask 255.255.255.0\n">>/etc/network/interfaces
+
+#/etc/network/interfacesの修正(削除)
+sudo cat /etc/network/interfaces|tr -d source-directory /etc/network/interfaces.d
+sudo cat /etc/network/interfaces|tr -d auto lo
+sudo cat /etc/network/interfaces|tr -d iface lo inet loopback
+sudo cat /etc/network/interfaces|tr -d iface eth0 inet manual
+sudo cat /etc/network/interfaces|tr -d auto wlan0
+sudo cat /etc/network/interfaces|tr -d allow-hotplug wlan0
+sudo cat /etc/network/interfaces|tr -d iface wlan0 inet static
+sudo cat /etc/network/interfaces|tr -d address 192.168.7.1
+sudo cat /etc/network/interfaces|tr -d netmask 255.255.255.0
+sudo cat /boot/cmdline.txt
+
 sudo systemctl stop  hostapd
 sudo systemctl disable  hostapd
 
