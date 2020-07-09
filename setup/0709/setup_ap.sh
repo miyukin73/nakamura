e2534a8d29fba36e6c105fe518c1e34fa44655a4
miyukin73 <nmiyuki73@keio.jp>
2020-07-09 23:23:59 +0900

    setup,off変更

---
+++ setup/0709/setup_ap.sh
@@ -0,0 +1,33 @@
+#RPiのAP化初回セットアップ
+#<<実行方法>>
+#bash setup_ap.sh
+
+#network/interfacesファイルに以下の項目を追加
+sudo sh -c "echo 'source-directory /etc/network/interfaces.d\nauto lo\niface lo inet loopback\niface eth0 inet manual\nauto wlan0\nallow-hotplug wlan0\niface wlan0 inet static\naddress 192.168.7.1\nnetmask 255.255.255.0'>>/etc/network/interfaces"
+#echoだけだと'許可がありません'のエラーが発生した　参考→https://linuxfan.info/post-1818
+
+
+#hostapdのインストール
+sudo apt-get install hostapd
+#hostapd/hostapd.confファイルに以下の項目を追加
+sudo sh -c "echo 'interface=wlan0\ndriver=nl80211\nssid=wolvez2020\nhw_mode=g\nchannel=6\nieee80211n=1\nwmm_enabled=1\nht_capab=[HT40][SHORT-GI-20][DSSS_CCK-40]\nmacaddr_acl=0\nauth_algs=1\nignore_broadcast_ssid=0\nwpa=2\nwpa_key_mgmt=WPA-PSK\nwpa_passphrase=wolvez2020\nrsn_pairwise=CCMP'>>/etc/hostapd/hostapd.conf"
+#↑ssidとpassphraseは適当に変えてください
+
+#/etc/default/hostapd を編集
+#もしかしたらデフォルトでなにか書いてあってそれ消す必要あるかも...?
+sudo sh -c 'echo "DAEMON_CONF="/etc/hostapd/hostapd.conf""'
+
+
+#dnsmasqのインストール
+sudo apt-get install dnsmasq
+#オリジナル設定ファイルのバックアプ
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+#dnsmasq.confファイルに以下の項目を追加
+sudo sh -c "echo 'interface=wlan0\nlisten-address=192.168.7.1\nbind-interfaces\nserver=8.8.8.8\nserver=8.8.4.4\ndomain-needed\nbogus-priv\ndhcp-range=192.168.7.100,192.168.7.199,24h'>>/etc/dnsmasq.conf"
+
+
+#設定完了の喜びの舞
+echo -e "<<設定完了のおしらせ>>\nおめでとう！設定完了だ！ともに高みへゆこう\n10秒後にラズパイを再起動します。"
+
+#reboot after 10s
+sudo reboot
